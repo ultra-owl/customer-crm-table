8d700cf6bd0f0916290f4a289984d79fee777a5f
ultra-owl <owl.ultimate001@gmail.com>
2025-11-29 03:50:15 +0530

    Update vite.config.js
    deployement

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// vite.config.js
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: '/customer-crm-table/',   // REQUIRED for GitHub Pages
+  base: "/customer-crm-table/",
 });
